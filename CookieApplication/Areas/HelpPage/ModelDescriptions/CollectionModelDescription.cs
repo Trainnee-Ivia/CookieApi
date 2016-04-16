adcfb2391d792d525d172ba244d8a8be2949832b
Michel <xexel.mq@gmail.com>
2016-04-16 03:08:27 -0300

    bug dos arquivos
    Retirado projeto desconhecido CookieApplication

--- CookieApplication/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CookieApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- CookieApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CookieApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
